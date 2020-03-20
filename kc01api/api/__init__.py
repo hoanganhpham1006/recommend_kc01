18df5a530f6287ae75d349aeab0a58c1be3128f3
Pham Hoang Anh <phamhoanganh@Phams-MacBook-Pro.local>
2020-03-20 22:30:37 +0700

    Add most model

--- kc01api/api/__init__.py
+++ kc01api/api/__init__.py
@@ -5,7 +5,7 @@ def recall20(y_true, y_pred, k=20, **kwargs):
     return tf.keras.metrics.sparse_top_k_categorical_accuracy(y_true, y_pred, k=k)
 
 print("Restoring DTER Model ..")
-dter_model = tf.keras.models.load_model('ptit_19Feb', custom_objects={"recall20": recall20}, compile=False)
+dter_model = tf.keras.models.load_model('most_19Feb', custom_objects={"recall20": recall20}, compile=False)
 
 dter_model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
@@ -15,4 +15,4 @@ dter_model.predict(test_input)
 print("Restored successfully DTER Model!")
 
 map_url_trainid = pickle.load(open('map_url_trainid.pkl', 'rb'))
-remap_info = pickle.load(open('remap_info.pkl', 'rb'))
+remap_info = pickle.load(open('remap_info_most.pkl', 'rb'))
--- kc01api/api/helpers/dter_inference_helper.py
+++ kc01api/api/helpers/dter_inference_helper.py
@@ -2,11 +2,11 @@ from api import dter_model, remap_info, map_url_trainid, tf
 import numpy as np
 
 def processing(list_url):
-    list_url = [map_url_trainid[url[:-1]] for url in list_url.split() if url[:-1] in map_url_trainid]
+    list_url = [map_url_trainid[url] for url in list_url.split() if url in map_url_trainid]
     print(list_url)
     list_url = tf.keras.preprocessing.sequence.pad_sequences(np.expand_dims(list_url, 0), maxlen=20, dtype='int64', padding='pre', truncating='pre', value=0)
     softmax = dter_model.predict(list_url)
-    results = softmax[0].argsort()[-21:][::-1]
+    results = softmax[0].argsort()[-22:][::-1]
     print(results)
-    list_rec = [remap_info[nid] for nid in list(results) if nid != 0]
+    list_rec = [remap_info[nid] for nid in list(results) if (nid != 0 and nid != list_url[0][-1])]
     return list_rec[:20]
