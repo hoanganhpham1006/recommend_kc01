07811b66ccd01c8566e5c3acfbe912bbde01fb7c
Pham Hoang Anh <phamhoanganh@Phams-MacBook-Pro.local>
2020-03-23 21:03:50 +0700

    Retrain model with additional data

---
+++ data/additional_data_all.sh
@@ -0,0 +1,29 @@
+echo '17330, "Cục Năng lượng nguyên tử tổ chức Lễ trao Quyết định bổ nhiệm, bổ nhiệm lại lãnh đạo một số đơn vị trực thuộc Cục
+", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17482, "Thủ tướng chỉ thị tạo điều kiện cho doanh nghiệp khởi nghiệp sáng tạo", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17497, "Mở thêm đường cho startup ra biển lớn", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17522, "Đưa chất xám xuống tuyến huyện, sản xuất hàng loạt test-kit xét nghiệm “made in Vietnam”", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17528, "Thông báo tuyển chọn tổ chức và cá nhân chủ trì thực hiện nhiệm vụ khoa học và công nghệ theo Nghị định thư với I-xra-en", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17538, "Thông báo tuyển chọn tổ chức và cá nhân chủ trì thực hiện nhiệm vụ khoa học và công nghệ cấp quốc gia thực hiện trong kế hoạch năm 2020 (theo Quyết định số 543/QĐ-BKHCN ngày 06/3/2020)", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17539, "Thông báo tuyển chọn tổ chức và cá nhân chủ trì thực hiện nhiệm vụ khoa học và công nghệ cấp quốc gia thực hiện trong kế hoạch năm 2020 (Quyết định số 542/QĐ-BKHCN ngày 06/3/2020)
+", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17541, "Báo cáo kết quả tự đánh giá nhiệm vụ KH&CN cấp quốc gia: Nghiên cứu, phát triển tích hợp hệ thống hỗ trợ giám sát, quản lý, vận hành an toàn cho hệ thống mạng và hạ tầng cung cấp dịch vụ công trực tuyến, Mã số: KC.01.01/16-20", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17545, "Thông báo về việc chỉnh sửa thuyết minh đề cương và ký hợp đồng thực hiện đề tài NCCB trong lĩnh vực KHTN&KT", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17546, "Thông báo về việc chỉnh sửa thuyết minh đề cương và ký hợp đồng thực hiện đề tài tiềm năng do Quỹ tài trợ thực hiện từ năm 2020", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17556, "Siêu máy tính chẩn đoán nCoV trong 10 giây", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17569, "Thông báo thực hiện Quyết định số 578/QĐ-BKHCN ngày 09/03/2020 của Bộ trưởng Bộ KH&CN về việc phê duyệt nhiệm vụ KH&CN cấp Bộ xét giao trực tiếp bắt đầu thực hiện từ năm 2020", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17573, "Thông báo về việc tuyển chọn tổ chức chủ trì và cá nhân chủ nhiệm thực hiện nhiệm vụ khoa học và công nghệ cấp quốc gia (Quyết định số 644/QĐ-BKHCN ngày 12/3/2020)", "", "", ""\n' >> post_MostPortal_all.csv
+
+echo '17575, "Các bài nghiên cứu mới về COVID-19 từ ngày 13/3 đến ngày 19/3/2020", "", "", ""\n' >> post_MostPortal_all.csv
