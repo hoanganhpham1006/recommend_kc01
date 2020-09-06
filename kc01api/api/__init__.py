635cafcaf72960b524d31ac9fb106bbc2d919229
Pham Hoang Anh <phamhoanganh@Phams-MacBook-Pro.local>
2020-09-06 18:27:40 +0700

    Check is file exist

--- kc01api/api/__init__.py
+++ kc01api/api/__init__.py
@@ -14,29 +14,30 @@ supported_dataset = ['MostPortal', 'QNPortal']
 def recall20(y_true, y_pred, k=20, **kwargs):
     return tf.keras.metrics.sparse_top_k_categorical_accuracy(y_true, y_pred, k=k)
 
-with open(settings.BASE_DIR + "/api/logs/model_log.txt", "r") as f:
-    lines = f.readlines()
+if os.path.isfile(settings.BASE_DIR + "/api/logs/model_log.txt"):
+    with open(settings.BASE_DIR + "/api/logs/model_log.txt", "r") as f:
+        lines = f.readlines()
 
-for l in lines:
-    status[l.split('/')[0]] = 1
-    lastest[l.split('/')[0]] = l[:-1]
+    for l in lines:
+        status[l.split('/')[0]] = 1
+        lastest[l.split('/')[0]] = l[:-1]
 
-for dataset in lastest.keys():
-    lastest_model_path[dataset] = settings.BASE_DIR + "/api/models/" + lastest[dataset]
-    lastest_data_path[dataset] = '_'.join(lastest[dataset].split('/')[-1].split('_')[1:])
+    for dataset in lastest.keys():
+        lastest_model_path[dataset] = settings.BASE_DIR + "/api/models/" + lastest[dataset]
+        lastest_data_path[dataset] = '_'.join(lastest[dataset].split('/')[-1].split('_')[1:])
 
-    print("Restoring DTER Model .. " + lastest[dataset])
-    dter_model[dataset] = tf.keras.models.load_model(lastest_model_path[dataset], custom_objects={"recall20": recall20}, compile=False)
+        print("Restoring DTER Model .. " + lastest[dataset])
+        dter_model[dataset] = tf.keras.models.load_model(lastest_model_path[dataset], custom_objects={"recall20": recall20}, compile=False)
 
-    dter_model[dataset].compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=[recall20])
-    test_input = tf.keras.preprocessing.sequence.pad_sequences([[1, 1]], maxlen=20, dtype='int64', padding='pre', truncating='pre', value=0)
-    dter_model[dataset].predict(test_input)
-    print("Restored successfully DTER Model! " + lastest[dataset])
+        dter_model[dataset].compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=[recall20])
+        test_input = tf.keras.preprocessing.sequence.pad_sequences([[1, 1]], maxlen=20, dtype='int64', padding='pre', truncating='pre', value=0)
+        dter_model[dataset].predict(test_input)
+        print("Restored successfully DTER Model! " + lastest[dataset])
 
-    map_url_trainid[dataset] = pickle.load(open(settings.BASE_DIR + "/api/databases/" + lastest[dataset].split('/')[0] + '/map_url_trainid_' + lastest_data_path[dataset] + '.pkl', 'rb'))
-    remap_info[dataset] = pickle.load(open(settings.BASE_DIR + "/api/databases/" + lastest[dataset].split('/')[0] + '/remap_info_' + lastest_data_path[dataset] + '.pkl', 'rb'))
+        map_url_trainid[dataset] = pickle.load(open(settings.BASE_DIR + "/api/databases/" + lastest[dataset].split('/')[0] + '/map_url_trainid_' + lastest_data_path[dataset] + '.pkl', 'rb'))
+        remap_info[dataset] = pickle.load(open(settings.BASE_DIR + "/api/databases/" + lastest[dataset].split('/')[0] + '/remap_info_' + lastest_data_path[dataset] + '.pkl', 'rb'))
     
 # #Statistic process
 # most_popular_results = [l.split(' | ') for l in open(settings.BASE_DIR + '/mostpopular.txt', 'rt', encoding='utf-8').readlines()]
