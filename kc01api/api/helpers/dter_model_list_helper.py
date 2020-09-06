ac1c2ae0cdddc48f74e95f2a27825e54b3b9f7ec
Pham Hoang Anh <phamhoanganh@Phams-MacBook-Pro.local>
2020-09-06 18:49:55 +0700

    Fix typo

--- kc01api/api/helpers/dter_model_list_helper.py
+++ kc01api/api/helpers/dter_model_list_helper.py
@@ -6,11 +6,11 @@ def processing(dataset_name):
         return ""
     current_model = ""
 
-    if os.path.isfie(settings.BASE_DIR + "/api/logs/model_log.txt"):
+    if os.path.isfile(settings.BASE_DIR + "/api/logs/model_log.txt"):
         with open(settings.BASE_DIR + "/api/logs/model_log.txt", "r") as f:
             lines = f.readlines()
         for l in lines:
             if l.split('/')[0] == dataset_name:
                 current_model = l.split('/')[1][:-1]
-                
+
     return {"list": os.listdir(settings.BASE_DIR + "/api/models/" + dataset_name), "current": current_model}
